6584f4c9e3e9f39724810b28373719390744f3dc
Jihye Pak <jiji.pak41@gmail.com>
2021-07-24 22:08:53 +0100

    create code-challenge3

---
+++ challenge3.js
@@ -0,0 +1,17 @@
+//Implement a function that calculates the sum of the integers inside a string.
+
+function sumOfIntegersInString(s) {
+  return;
+}
+
+console.log(
+  sumOfIntegersInString(
+    'The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog'
+  )
+); // => 3635
+
+// function sumOfIntegersInString(s){
+//   let regex = /\d+/g
+
+//   return s.match(regex).reduce((a, c) => Number(a) + Number(c), 0)
+// }
