6bc27ab3744001b694c4fe812ba1207c2909813f
jihye <jiji.pak41@gmail.com>
2021-09-03 23:38:34 +0100

    fix function name add third solution

--- challenge6.js
+++ challenge6.js
@@ -3,7 +3,7 @@
 // from codewars => https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
 
 // first solution
-const isSquare = function (n) {
+const isSquare1 = function (n) {
   if (n < 0) return false;
   if (n === 0 || n === 1) return true;
   for (let i = 0; i < n; i++) {
@@ -16,6 +16,13 @@ const isSquare = function (n) {
 };
 
 // second solution
-constisSquare = function (n) {
+const isSquare2 = function (n) {
   return Number.parseInt(Math.sqrt(n)) ** 2 === n;
 };
+
+// third solution
+const isSquare3 = function (n) {
+  return Number.isInteger(Math.sqrt(n));
+};
+
+console.log(isSquare3(5));
